483d27791428d1bdf70058bf23c22410a5754cda
Maxime Mons <pro.maxime.mons@gmail.com>
2025-07-08 00:43:00 +0200

    [fix] Miss translations on index.html

--- js/langs.js
+++ js/langs.js
@@ -2,7 +2,7 @@ const translations = {
   en: {
     title: "Baby Roulette",
     cardGenderRevealTitle: "Gender Reveal",
-    cardGenderRevealDescription: "Slot machine to reveal the bab's gender",
+    cardGenderRevealDescription: "Slot machine to reveal the baby's gender",
     cardNameRevealTitle: "Name Reveal",
     cardNameRevealDescription: "\"Wordle\" type game to reveal the baby's name",
     cardNameTournamentTitle: "Names tournament",
@@ -13,43 +13,53 @@ const translations = {
     cardGenderRevealTitle: "Bandit Manchot",
     cardGenderRevealDescription: "Jeu type machine à sous qui permet de révéler le sexe de bébé",
     cardNameRevealTitle: "Motus",
-    cardNameRevealDescription: "Jeu type \"Motus\" qui permer de révéler le prénom de bébé",
+    cardNameRevealDescription: "Jeu type \"Motus\" qui permet de révéler le prénom de bébé",
     cardNameTournamentTitle: "Tournoi de noms",
-    cardNameTournamentDescription: "Bataille entre prenoms pour déterminer le bon"
+    cardNameTournamentDescription: "Bataille entre prénoms pour déterminer le bon"
   },
   es: {
     title: "Baby Roulette",
-    cardGenderRevealTitle: "Gender Reveal",
-    cardGenderRevealDescription: "",
-    cardNameRevealTitle: "Name Reveal",
-    cardNameRevealDescription: ""
+    cardGenderRevealTitle: "Revelación de género",
+    cardGenderRevealDescription: "Máquina tragamonedas para revelar el género del bebé",
+    cardNameRevealTitle: "Revelación de nombre",
+    cardNameRevealDescription: "Juego tipo \"Wordle\" para revelar el nombre del bebé",
+    cardNameTournamentTitle: "Torneo de nombres",
+    cardNameTournamentDescription: "Batallas entre nombres para encontrar el mejor"
   },
   de: {
     title: "Baby Roulette",
-    cardGenderRevealTitle: "Gender Reveal",
-    cardGenderRevealDescription: "",
-    cardNameRevealTitle: "Name Reveal",
-    cardNameRevealDescription: ""
+    cardGenderRevealTitle: "Geschlecht enthüllen",
+    cardGenderRevealDescription: "Spielautomat, um das Geschlecht des Babys zu enthüllen",
+    cardNameRevealTitle: "Namensenthüllung",
+    cardNameRevealDescription: "Ein \"Wordle\"-ähnliches Spiel, um den Namen des Babys zu enthüllen",
+    cardNameTournamentTitle: "Namens-Turnier",
+    cardNameTournamentDescription: "Kämpfe zwischen Namen, um den besten zu finden"
   },
   it: {
     title: "Baby Roulette",
-    cardGenderRevealTitle: "Gender Reveal",
-    cardGenderRevealDescription: "",
-    cardNameRevealTitle: "Name Reveal",
-    cardNameRevealDescription: ""
+    cardGenderRevealTitle: "Rivelazione del sesso",
+    cardGenderRevealDescription: "Slot machine per rivelare il sesso del bambino",
+    cardNameRevealTitle: "Rivelazione del nome",
+    cardNameRevealDescription: "Gioco tipo \"Wordle\" per rivelare il nome del bambino",
+    cardNameTournamentTitle: "Torneo dei nomi",
+    cardNameTournamentDescription: "Battaglie tra nomi per trovare il migliore"
   },
   pt: {
     title: "Baby Roulette",
-    cardGenderRevealTitle: "Gender Reveal",
-    cardGenderRevealDescription: "",
-    cardNameRevealTitle: "Name Reveal",
-    cardNameRevealDescription: ""
+    cardGenderRevealTitle: "Revelação do gênero",
+    cardGenderRevealDescription: "Máquina caça-níqueis para revelar o gênero do bebê",
+    cardNameRevealTitle: "Revelação do nome",
+    cardNameRevealDescription: "Jogo tipo \"Wordle\" para revelar o nome do bebê",
+    cardNameTournamentTitle: "Torneio de nomes",
+    cardNameTournamentDescription: "Batalhas entre nomes para encontrar o melhor"
   },
   nl: {
     title: "Baby Roulette",
-    cardGenderRevealTitle: "Gender Reveal",
-    cardGenderRevealDescription: "",
-    cardNameRevealTitle: "Name Reveal",
-    cardNameRevealDescription: ""
+    cardGenderRevealTitle: "Geslacht onthulling",
+    cardGenderRevealDescription: "Gokkast om het geslacht van de baby te onthullen",
+    cardNameRevealTitle: "Naam onthulling",
+    cardNameRevealDescription: "\"Wordle\"-achtig spel om de naam van de baby te onthullen",
+    cardNameTournamentTitle: "Naam toernooi",
+    cardNameTournamentDescription: "Gevechten tussen namen om de beste te vinden"
   }
 };
